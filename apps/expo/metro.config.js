866879f4b11639e6769b7ef7e15b4e422d6938a2
KaviiSuri <kavii@speechify.com>
2024-06-22 14:17:46 +0530

    fix: removed ui

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -1,17 +1,11 @@
 // Learn more: https://docs.expo.dev/guides/monorepos/
 const { getDefaultConfig } = require("expo/metro-config");
 const { FileStore } = require("metro-cache");
-const { withNativeWind } = require("nativewind/metro");
 
 const path = require("path");
 
 module.exports = withTurborepoManagedCache(
-  withMonorepoPaths(
-    withNativeWind(getDefaultConfig(__dirname), {
-      input: "./src/styles.css",
-      configPath: "./tailwind.config.ts",
-    }),
-  ),
+  withMonorepoPaths(getDefaultConfig(__dirname)),
 );
 
 /**

f848b6c5204647b43546e97e34c4daabd8f4c0e1
KaviiSuri <kavii@speechify.com>
2024-06-29 15:31:15 +0530

    fix: added database token for turso

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -4,7 +4,10 @@ import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema.js";
 
 const turso = createClient({
-  url: "file:local.db",
+  // eslint-disable-next-line no-restricted-properties, @typescript-eslint/no-non-null-assertion
+  url: process.env.DATABASE_URL!,
+  // eslint-disable-next-line no-restricted-properties
+  authToken: process.env.DATABASE_TOKEN,
 });
 
 export const db = drizzle(turso, {

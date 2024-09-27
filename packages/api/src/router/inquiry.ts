59a0d4e301b2bdba773c4b0518e0da1e33237046
KaviiSuri <surikavii@gmail.com>
2024-09-27 13:41:43 +0530

    fix: build issues

--- packages/api/src/router/inquiry.ts
+++ packages/api/src/router/inquiry.ts
@@ -1,10 +1,10 @@
 import type { TRPCRouterRecord } from "@trpc/server";
 import { TRPCError } from "@trpc/server";
-import { protectedProcedure } from "../trpc";
+import { protectedProcedure } from "../trpc.js";
 import { z } from "zod";
 import { inquiries } from "@repo/db/schema";
 import { nanoid } from "nanoid";
-import { productRequestSchema, quotesService } from "../services/quote";
+import { productRequestSchema, quotesService } from "../services/quote.js";
 import { and, eq, or, lte, lt } from "@repo/db";
 
 // Define the schema for raising an inquiry

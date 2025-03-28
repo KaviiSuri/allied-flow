564d9318d8e265d84b62ab09bca781754430a645
AirSurfer09 <saurav@convai.com>
2025-03-28 17:40:43 +0530

    logs

--- apps/expo/src/components/dashboard/DashboardPage.web.tsx
+++ apps/expo/src/components/dashboard/DashboardPage.web.tsx
@@ -8,8 +8,21 @@ import { api } from "~/utils/api";
 import { useEffect, useState } from "react";
 
 export const DashboardPage = () => {
+  const [end_date, setEndDate] = useState(new Date());
+  const [start_date, setStartDate] = useState(() => {
+    const date = new Date();
+    date.setDate(date.getDate() - 30);
+    return date;
+  });
 
+  const { data: summaryData } = api.analytics.getSummary.useQuery({
+    start_date,
+    end_date,
+  });
 
+  useEffect(() => {
+    console.log(summaryData);
+  }, [summaryData]);
 
   return (
     <View style={dashboardWebStyles.container}>
@@ -19,29 +32,29 @@ export const DashboardPage = () => {
         <View style={dashboardWebStyles.singleSection}>
           <Text style={dashboardWebStyles.sectionHeader}>Summary</Text>
           <View style={dashboardWebStyles.boardSectionContainer}>
-            <BoardSection 
-              title="Total Revenue" 
-              score={summaryData?.sales.revenue.value.toString() ?? "0"} 
+            <BoardSection
+              title="Total Revenue"
+              score={summaryData?.sales.revenue.value.toString() ?? "0"}
             />
-            <BoardSection 
-              title="Sample Orders" 
-              score={summaryData?.sales.samples.count.toString() ?? "0"} 
+            <BoardSection
+              title="Sample Orders"
+              score={summaryData?.sales.samples.count.toString() ?? "0"}
             />
-            <BoardSection 
-              title="Total Inquiries" 
-              score={summaryData?.inquiries.received.total.toString() ?? "0"} 
+            <BoardSection
+              title="Total Inquiries"
+              score={summaryData?.inquiries.received.total.toString() ?? "0"}
             />
-            <BoardSection 
-              title="Win Rate" 
-              score={`${Math.round(summaryData?.inquiries.received.winPercentage ?? 0)}%`} 
+            <BoardSection
+              title="Win Rate"
+              score={`${Math.round(summaryData?.inquiries.received.winPercentage ?? 0)}%`}
             />
-            <BoardSection 
-              title="Inquiry Value" 
-              score={summaryData?.inquiries.value.total.toString() ?? "0"} 
+            <BoardSection
+              title="Inquiry Value"
+              score={summaryData?.inquiries.value.total.toString() ?? "0"}
             />
-            <BoardSection 
-              title="Value Win Rate" 
-              score={`${Math.round(summaryData?.inquiries.value.winPercentage ?? 0)}%`} 
+            <BoardSection
+              title="Value Win Rate"
+              score={`${Math.round(summaryData?.inquiries.value.winPercentage ?? 0)}%`}
             />
           </View>
 

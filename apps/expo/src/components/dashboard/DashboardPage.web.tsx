031b0c5b386ceaba4a83c317c9304a9b6f8a95ef
AirSurfer09 <saurav@convai.com>
2025-03-28 14:28:46 +0530

    stash

--- apps/expo/src/components/dashboard/DashboardPage.web.tsx
+++ apps/expo/src/components/dashboard/DashboardPage.web.tsx
@@ -2,15 +2,12 @@ import { Text, View } from "react-native";
 import { dashboardWebStyles } from "./css";
 import { BoardSection } from "./boardSection";
 import { api } from "~/utils/api";
+import { useEffect, useState } from "react";
 
 export const DashboardPage = () => {
-  const { data, isLoading, error } = api.analytics.getSummary.useQuery({
-    start_date: new Date(new Date().setMonth(-12)),
-    end_date: new Date(),
-  });
-  console.log("analytics data", data);
-  console.log("isLoading", isLoading);
-  console.log("error", error);
+
+
+
   return (
     <View style={dashboardWebStyles.container}>
       {/* three small boards with info */}
